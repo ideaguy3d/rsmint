4f63537c6bacfae1fa054563beabc41db9f19514
Julius Alvarado <javascript.uiux@gmail.com>
2018-10-08 11:43:05 -0700

    updated composer.lock

--- app/src/users/UserController.js
+++ app/src/users/UserController.js
@@ -1,89 +1,91 @@
-(function(){
-
-  angular
-       .module('users')
-       .controller('UserController', [
-          'userService', '$mdSidenav', '$mdBottomSheet', '$timeout', '$log',
-          UserController
-       ]);
-
-  /**
-   * Main Controller for the Angular Material Starter App
-   * @param $mdSidenav
-   * @param avatarsService
-   * @constructor
-   */
-  function UserController( userService, $mdSidenav, $mdBottomSheet, $timeout, $log ) {
-    const self = this;
-
-    self.selected     = null;
-    self.users        = [ ];
-    self.selectUser   = selectUser;
-    self.toggleList   = toggleUsersList;
-    self.makeContact  = makeContact;
-
-    // Load all registered users
-
-    userService
-          .loadAllUsers()
-          .then( function( users ) {
-            self.users    = [].concat(users);
-            self.selected = users[0];
-          });
-
-    // *********************************
-    // Internal methods
-    // *********************************
+(function () {
+    angular.module('users')
+        .controller('UserController', [
+            'userService', '$mdSidenav', '$mdBottomSheet', '$timeout', '$log',
+            UserControllerClass
+        ]);
 
     /**
-     * Hide or Show the 'left' sideNav area
+     * Main Controller for the Angular Material Starter App
+     *
+     * @param userService
+     * @param $mdSidenav
+     * @param $mdBottomSheet
+     * @param $timeout
+     * @param $log
+     *
+     * @constructor
      */
-    function toggleUsersList() {
-      $mdSidenav('left').toggle();
-    }
+    function UserControllerClass(userService, $mdSidenav, $mdBottomSheet, $timeout, $log) {
+        const self = this;
 
-    /**
-     * Select the current avatars
-     * @param menuId
-     */
-    function selectUser ( user ) {
-      self.selected = angular.isNumber(user) ? self.users[user] : user;
-    }
+        self.selected = null;
+        self.users = [];
+        self.selectUser = selectUser;
+        self.toggleList = toggleUsersList;
+        self.makeContact = makeContact;
 
-    /**
-     * Show the Contact view in the bottom sheet
-     */
-    function makeContact(selectedUser) {
+        // Load all registered users
+        userService
+            .loadAllUsers()
+            .then(function (users) {
+                self.users = [].concat(users);
+                self.selected = users[0];
+            });
 
-        $mdBottomSheet.show({
-          controllerAs  : "vm",
-          templateUrl   : './src/users/view/contactSheet.html',
-          controller    : [ '$mdBottomSheet', ContactSheetController],
-          parent        : angular.element(document.getElementById('content'))
-        }).then(function(clickedItem) {
-          $log.debug( clickedItem.name + ' clicked!');
-        });
+        // *********************************
+        // ******* Internal methods *******
+        // *********************************
 
         /**
-         * User ContactSheet controller
+         * Hide or Show the 'left' sideNav area
          */
-        function ContactSheetController( $mdBottomSheet ) {
-          this.user = selectedUser;
-          this.items = [
-            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
-            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
-            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
-            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
-          ];
-          this.contactUser = function(action) {
-            // The actually contact process has not been implemented...
-            // so just hide the bottomSheet
-
-            $mdBottomSheet.hide(action);
-          };
+        function toggleUsersList() {
+            $mdSidenav('left').toggle();
         }
-    }
 
-  }
+        /**
+         * Select the current avatars
+         * @param menuId
+         */
+        function selectUser(user) {
+            self.selected = angular.isNumber(user) ? self.users[user] : user;
+        }
+
+        /**
+         * Show the Contact view in the bottom sheet
+         */
+        function makeContact(selectedUser) {
+
+            $mdBottomSheet.show({
+                controllerAs: "vm",
+                templateUrl: './src/users/view/contactSheet.html',
+                controller: ['$mdBottomSheet', ContactSheetController],
+                parent: angular.element(document.getElementById('content'))
+            }).then(function (clickedItem) {
+                $log.debug(clickedItem.name + ' clicked!');
+            });
+
+            /**
+             * User ContactSheet controller
+             */
+            function ContactSheetController($mdBottomSheet) {
+                this.user = selectedUser;
+                this.items = [
+                    {name: 'Phone', icon: 'phone', icon_url: 'assets/svg/phone.svg'},
+                    {name: 'Twitter', icon: 'twitter', icon_url: 'assets/svg/twitter.svg'},
+                    {name: 'Google+', icon: 'google_plus', icon_url: 'assets/svg/google_plus.svg'},
+                    {name: 'Hangout', icon: 'hangouts', icon_url: 'assets/svg/hangouts.svg'}
+                ];
+                this.contactUser = function (action) {
+                    // The actually contact process has not been implemented...
+                    // so just hide the bottomSheet
+
+                    $mdBottomSheet.hide(action);
+                };
+            }
+        }
+
+    } // END OF: UserControllerClass
 
 })();
