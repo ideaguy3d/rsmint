c28928a7079d38a191aca912e08a04a71f636478
Julius Alvarado <javascript.uiux@gmail.com>
2018-10-18 21:29:59 -0700

    initial success of 1st phase of b auth

--- app/states/auth/ctrl.auth.js
+++ app/states/auth/ctrl.auth.js
@@ -16,11 +16,25 @@
             console.log("should make request to Ninja API");
             rsmAuthSer.rsmLogin(vm.userDataModel)
                 .then(function (res) {
-                    console.log("Server Response:");
+                    console.log("------------------------------------------------");
                     console.log(res);
+                    let usernameRes = res.data["x-rsm-result-set"][0]['username'];
+
+                    console.log("------------------------------------------------");
+                    console.log("usernameRes = " + usernameRes);
+                    if (usernameRes === vm.userDataModel.username) {
+                        let qqqRes = res.data["x-rsm-result-set"][0]['rsm_access_Q'];
+                        if (qqqRes === vm.userDataModel.qqq) {
+                            console.log("RSM_SUCCESS - user is good to go :)");
+                        }
+                    }
+                    else {
+                        console.log("------------------------------------------------");
+                        console.log("RSM_ERROR - user not valid >:(");
+                    }
                 })
                 .catch(function (err) {
-                    console.error("RSM_ERROR: ", err);
+                    console.log("__>> RSM_ERROR: ", err);
                 });
         };
     }
