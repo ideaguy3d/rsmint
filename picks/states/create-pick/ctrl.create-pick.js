5532ffc13e8af3f5ea1086cc55a4bbcca93e2645
Julius Alvarado <javascript.uiux@gmail.com>
2018-11-12 13:40:31 -0800

    ui form almost complete

--- picks/states/create-pick/ctrl.create-pick.js
+++ picks/states/create-pick/ctrl.create-pick.js
@@ -9,6 +9,34 @@
     function CreatePickCtrlClass($rootScope, $scope, $mdToast, rsmPickService) {
         const vm = this;
         vm.viewTitle = "Create a Pick ";
+
+        //-- View model data bindings:
+        vm.coordinatorName = '';        // 1
+        vm.dueDate = '';                // 2
+        vm.quantity = '';
+        vm.paperDescription = '';
+        vm.envelopeDescription = '';
+        vm.paperImgUrl = '';
+        vm.envelopeImgUrl = '';
+        vm.jobNumber = '';
+
+        let pickTicketModel = {
+            coordinatorName: vm.coordinatorName,
+            dueDate: vm.dueDate,
+            quantity: vm.quantity,
+            paperDescription: vm.paperDescription,
+            envelopeDescription: vm.envelopeDescription,
+            paperImgUrl: vm.paperImgUrl,
+            envelopeImgUrl: vm.envelopeImgUrl
+        };
+
+        vm.sendPickTicket = sendPickTicket;
+
+        function sendPickTicket() {
+            console.log("going to send this request:");
+            console.log(pickTicketModel);
+        }
+
     }
 
 }());
